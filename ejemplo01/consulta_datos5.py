bbf97bba4210ca9b8c20dc29e21864a7f2948269
santyago0 <santyago0515@gmail.com>
2025-05-20 09:05:25 -0500

    consultas complejas

--- ejemplo01/consulta_datos5.py
+++ ejemplo01/consulta_datos5.py
@@ -19,19 +19,36 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # Obtener un listado de todos los registros 
-# de la tabla Club, que tengan al menos 
+# de la tabla Club y Jugador, que tengan al menos 
 # un jugador con el nombre que tenga incluida la cadena “Da”
 
 # para la solución se hace uso del método 
 # join aplicado a query
+# en el query se ubican las dos entidades involucradas
+# 
 
-clubs = session.query(Club).join(Jugador).\
-        filter(Jugador.nombre.like("%Da%")).all()
-# print(clubs)
-print("Consulta 1 ")
+registros = session.query(Club, Jugador).join(Jugador).\
+         filter(Jugador.nombre.like("%Da%")).all()
+ 
+print("Consulta 2 ")
+# print(registros)
 """
-Consulta 1 
+
+Consulta 2 
+
 Club: nombre=Barcelona deporte=Fútbol fundación=1920
+Jugador: Damian Diaz - dorsal:10 - posición: mediocampo
+
+
+Club: nombre=Barcelona deporte=Fútbol fundación=1920
+Jugador: Dario Aymar - dorsal:2 - posición: defensa
 """
-for e in clubs: 
-    print(e) 
+for registro in registros: 
+#     # el registro continen 
+#     # dos valores en un tupla
+#     # posición 0 el club
+#     # posición 1 el jugador 
+#     # que cumplen con la condición
+    print(registro[0].nombre) # El club
+    print(registro[1].nombre) # El jugador
+    print("------------------")
